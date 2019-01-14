ffbfb47b97efdda6294e1263aea13f52a6c55370
Kishan <kkpatel911@gmail.com>
2019-01-14 13:32:20 -0500

    fixed issue with client_id

--- js/main.js
+++ js/main.js
@@ -3,18 +3,37 @@ $(document).ready(function () {
     var username = e.target.value;
 
     //Making Fetch Request
-    fetch('https://api.github.com/users/' + username,
-      {
-        client_id: "aafb44a9a6fd2be7b577",
-        client_secret: "24d4a63541c87a76f19d7bd074140b7c51784007"
-      })
+    fetch("https://api.github.com/users/"+ username+"?client_id=98501be021148392de94&client_secret=d5b8ca269c822c3d6908f610aa024214aa1fc452")
       .then(function (res) {
         return res.json();
       })
       .then(function (data) {
         $('#profile').html(`
-        ${data.name}
-        `)
+        <div class="card" style="width: 18rem;">
+         <img class="card-img-top" src="${data.avatar_url}" alt="Card image cap">
+         <div class="card-body">
+            <h5 class="card-title">${data.name}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+          `)
+
+        // <div class="panel panel-default">
+        //   <div class="panel-heading">
+        //     <h3 class="panel-title">${data.name}</h3>
+        //   </div>
+        //   <div class="panel-body">
+        //     <div class='row'>
+        //       <div class = "col-md-3">
+        //       <img class = 'thumbnail' src="${data.avatar_url}">
+        //     </div>
+
+        //     </div>
+        //    </div>
+        // </div>
+        //
       })
   })
 });
