db914d7fb7da9538f61010cd0757cd00278628b5
Kishan <kkpatel911@gmail.com>
2019-01-15 19:24:44 -0500

    added loop

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var username = e.target.value;
 
     //Making Fetch Request
-    fetch("https://api.github.com/users/"+ username+"?client_id=98501be021148392de94&client_secret=d5b8ca269c822c3d6908f610aa024214aa1fc452")
+    fetch("https://api.github.com/users/" + username + "?client_id=98501be021148392de94&client_secret=d5b8ca269c822c3d6908f610aa024214aa1fc452")
       .then(function (res) {
         return res.json();
       })
@@ -13,27 +13,54 @@ $(document).ready(function () {
          <img class="card-img-top" src="${data.avatar_url}" alt="Card image cap">
          <div class="card-body">
             <h5 class="card-title">${data.name}</h5>
+
+              <span class="badge badge-primary">Public Repos: ${data.public_repos}</span>
+              <span class="badge badge-secondary">Public Gists:${data.public_gists}</span>
+              <span class="badge badge-success">Followers: ${data.followers}</span>
+              <span class="badge badge-danger"> Following: ${data.following}</span>
+            <ul class="list-group pt-2 pb-2">
+              <li class="list-group-item">Company: ${data.company}</li>
+              <li class="list-group-item">Websites: ${data.blog}</li>
+              <li class="list-group-item">location: ${data.location}</li>
+              <li class="list-group-item">Email: ${data.email}</li>
+              <li class="list-group-item">Member Since: ${data.created_at}</li>
+            </ul>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
 
+        <h3 class='page-header'>Latest Repos</h3>
+        <div id='hell'>
+
+
+        </div>
+
           `)
-
-        // <div class="panel panel-default">
-        //   <div class="panel-heading">
-        //     <h3 class="panel-title">${data.name}</h3>
-        //   </div>
-        //   <div class="panel-body">
-        //     <div class='row'>
-        //       <div class = "col-md-3">
-        //       <img class = 'thumbnail' src="${data.avatar_url}">
-        //     </div>
-
-        //     </div>
-        //    </div>
-        // </div>
-        //
       })
   })
 });
+$(document).ready(function () {
+  $('#searchUser').on('keyup', function (e) {
+    var username = e.target.value;
+
+    fetch("https://api.github.com/users/" + username + "/repos?client_id=98501be021148392de94&client_secret=d5b8ca269c822c3d6908f610aa024214aa1fc452")
+      .then(function (infojson) {
+        return infojson.json();
+      })
+      .then(function (repos) {
+        repos.forEach(function (element) {
+          console.log(element.name);
+          $('#hell').append(`
+            <div>${element.name}</div>
+
+          `)
+
+
+
+        })
+      })
+
+  })
+
+})
